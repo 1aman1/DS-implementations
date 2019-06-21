75552152946d414f57d8ac149669921e186bb6d9
Aman Gupta <49602858+1aman1@users.noreply.github.com>
2019-06-21 13:47:05 +0530

    Update lenEvenOddSLL.cpp

--- snippetsAmidImplementation/lenEvenOddSLL.cpp
+++ snippetsAmidImplementation/lenEvenOddSLL.cpp
@@ -1,12 +1,10 @@
-int isLengthEvenOrOdd(struct Node* head)
-{
+int isLengthEvenOrOdd(struct Node* head) {
     Node* temp = head;
     int count = 0;
     while( temp ){
         ++count;
         temp = temp->next;
     }
-    //
 //cout << count << "here\n";
 return (1&count);
 }
