636cf3c9dfc72d5547566551fe0e062b3b38ad03
Aman Gupta <49602858+1aman1@users.noreply.github.com>
2019-06-21 13:48:29 +0530

    Update removeDup.cpp

--- snippetsAmidImplementation/removeDup.cpp
+++ snippetsAmidImplementation/removeDup.cpp
@@ -1,14 +1,13 @@
-Node *removeDuplicates(Node *root)
-{
-    Node* tmp = root;
-    Node* Del;
-    while( tmp->next != NULL ) {
-        if( tmp->data == tmp->next->data ) {
-            Del = tmp->next;
-            tmp->next = tmp->next->next;
-        }
-        else
-            tmp = tmp->next;
-        }
-    return root;
+Node *removeDuplicates(Node *root) {
+	Node* tmp = root;
+  Node* Del;
+  while( tmp->next != NULL ) {
+		if( tmp->data == tmp->next->data ) {
+			Del = tmp->next;
+			tmp->next = tmp->next->next;
+		}
+		else
+			tmp = tmp->next;
+	}
+	return root;
 }
